630f95666784681e7177f3739f68004f6e456cb6
kevinkjf02 <kevinkjf02@gmail.com>
2021-06-16 23:47:52 -0500

    Update Program.cs

--- MiPrimerApp.Consola/Program.cs
+++ MiPrimerApp.Consola/Program.cs
@@ -7,6 +7,7 @@ namespace MiPrimerApp.Consola
         static void Main(string[] args)
         {
             Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Hola Mundo");
         }
     }
 }
